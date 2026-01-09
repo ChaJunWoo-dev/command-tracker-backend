c91246a686085247024b9d85c4c216ef42b07242
ChaJunWoo <wnsdnxla123@gmail.com>
2026-01-10 02:02:41 +0900

    refactor: 상수 및 환경 설정 리팩토링
    - 미사용 상수 제거
    - 신규 상수 추가
    - 환경 변수 간소화

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,6 @@ const config = {
   port: process.env.PORT || 8000,
   node_env: process.env.NODE_ENV || "development",
   API_PREFIX: process.env.API_PREFIX || "/api",
-  ORIGINAL_PREFIX: process.env.ORIGINAL_PREFIX || "original",
-  EDITED_PREFIX: process.env.EDITED_PREFIX || "edited",
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   AWS_REGION: process.env.AWS_REGION,
@@ -11,8 +9,6 @@ const config = {
   rabbitmqUrl: process.env.RABBITMQ_URL,
   email_user: process.env.EMAIL_USER,
   email_pass: process.env.EMAIL_PASS,
-  emailQueue: process.env.EMAIL_QUEUE || "email_queue",
-  analyzeQueue: process.env.ANALYZE_QUEUE || "analyze_queue",
 };
 
 export default config;
